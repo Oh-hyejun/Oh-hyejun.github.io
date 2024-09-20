37c83b1838e05df350c22e61835a1654181423cf
Oh-hyejun <qwegw0303@naver.com>
2024-09-20 19:08:34 +0900

    디자인 대수정

--- src/App.js
+++ src/App.js
@@ -6,27 +6,22 @@ import gitIcon from './assets/images/gitIcon.png';
 function App() {
   return (
     <div className="App">
-      <div className='header_container'>
-        <div className='header'>
-          <p className='header_title'>오혜준 포트폴리오</p>
-          <div className='nav'>
-            <p className='nav_item'>나의소개</p>
-            <p className='nav_item'>나의기술</p>
-            <p className='nav_item'>프로젝트</p>
+      <div className='main_slide'>
+        <div className='text_container'>
+          <div className='line_text'>
+            <p>
+              안녕하세요.
+              <br />
+              끊임없이 <span className='white_font'>성장하는 개발자</span>
+              <br />
+              <span className='white_font'>오혜준</span>입니다.
+            </p>
           </div>
         </div>
-        <p style={{ fontSize: "100px" }}>
-          안녕하세요 <br/>
-          끊임없이 성장하는 개발자 <br/>
-          <span className='highlight'>오혜준</span>입니다
-        </p>
-        <div className='icon_container'>
-          <a href="https://ohhyejun33.tistory.com/" target="_blank" rel="noopener noreferrer">
-            <img className='icon' src={tStoryIcon} alt="티스토리 아이콘 입니다" />
-          </a>
-          <a href="https://github.com/Oh-hyejun" target="_blank" rel="noopener noreferrer">
-            <img className='icon' src={gitIcon} alt="깃허브 아이콘 입니다" />
-          </a>
+        <div className='category'>
+          <p>1. About Me</p>
+          <p>2. Skills</p>
+          <p>3. Project</p>
         </div>
       </div>
     </div>
