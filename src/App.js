5be050b56d0a99d0c7dbd0f2590ab00cd11e2266
Oh-hyejun <qwegw0303@naver.com>
2024-11-07 21:44:06 +0900

    modify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Portfolio_form from './js/portfolio_form';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio">
       <div className="App">
         <Routes>
-          <Route path='/portfolio' element={ <Portfolio_form></Portfolio_form> }></Route>
+          <Route path="/" element={<Portfolio_form />} />
         </Routes>
       </div>
     </Router>
